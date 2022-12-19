8b948121df36641805161d50df4ed700b8a2c21f
4CI-David-Roberto-Constantin-2022-2023 <90649857+4CI-David-Roberto-Constantin-2022-2023@users.noreply.github.com>
2022-12-19 21:59:33 +0100

    Delete 2022-2023/david_roberto_venditaarticolo/bin directory

--- 2022-2023/david_roberto_venditaarticolo/bin/ApplicazioneArticolo.java
+++
@@ -1,8 +0,0 @@
-package bin;
-public class ApplicazioneArticolo
-{
-    public static void main (String [] args){
-        VenditaArticolo vendita = new VenditaArticolo();
-        vendita.start();
-    }
-}
